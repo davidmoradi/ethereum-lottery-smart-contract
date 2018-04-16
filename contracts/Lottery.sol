59a8f49b436f9140bf38c68d9a8009d611105a84
davidmoradi <david@wishpond.com>
2018-04-15 21:23:29 -0700

    Add Lottery contract

---
+++ contracts/Lottery.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
+    }
+
+    function kindaRandom() private view returns (uint) {
+        return uint(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public restricted {
+        uint index = kindaRandom() % players.length;
+
+        // the entitre balance of this contract to the winner.
+        players[index].transfer(this.balance);
+
+        // clear players and start over.
+        players = new address[](0);
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+
+    // restrict to only the manager (the contract creator)
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+}
